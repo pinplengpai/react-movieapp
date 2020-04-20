ffdf1d2444617857b476eaf7ca4c11b248007c6d
Plengpai Ratnajarn <pinplengpai@gmail.com>
2020-04-20 14:52:05 +0700

    import antd, axios and create new folders & files

---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Input } from 'antd';
+
+
+
+function Search() {
+    return (
+       <section>
+           < Input/>
+
+       </section>
+    )
+}
+
+export default Search
